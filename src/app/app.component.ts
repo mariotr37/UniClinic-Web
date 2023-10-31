d6b3df13fffebc3255cf0d6439f19db46d645e08
Mario Tabares <marioa.tabaresr@uqvirtual.edu.co>
2023-10-30 22:56:32 -0500

    Configuración inicial del frontend con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'UniClinic-Web';
+  title = 'UniClinic';
+  year = new Date().getFullYear();
 }
