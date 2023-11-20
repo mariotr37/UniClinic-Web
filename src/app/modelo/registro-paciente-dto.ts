a764813cb6b3a9cc948ea86946aef6526f61fd14
sbastianr <rodriguezpalaciojuansebastian@gmail.com>
2023-11-20 12:11:23 -0500

    Se creo la clase RegistroDTO

---
+++ src/app/modelo/registro-paciente-dto.ts
@@ -0,0 +1,14 @@
+export class RegistroPacienteDTO {
+    cedula:string = "";
+    nombre:string = "";
+    telefono:string = "";
+    urlFoto:string = "";
+    ciudad:string = "";
+    fechaNacimiento:string = "";
+    alergias:string = "";
+    eps:string = "";
+    tipoSangre:string = "";
+    correo:string = "";
+    password:string = "";
+    confirmaPassword:string = "";
+}
--- src/app/pagina/registro/registro.component.ts
+++ src/app/pagina/registro/registro.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { RegistroPacienteDTO } from 'src/app/modelo/registro-paciente-dto';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
 
+export class RegistroComponent {
+  registroPacienteDTO: RegistroPacienteDTO;
+  ciudades:string[];
+
+  constructor(){
+    this.registroPacienteDTO = new RegistroPacienteDTO();
+    this.ciudades = [];
+    this.cargarCiudades();
+  }
+
+  public registrar(){
+    console.log(this.registroPacienteDTO);
+  }
+
+  public sonIguales():boolean{
+    return this.registroPacienteDTO.password == this.registroPacienteDTO.confirmaPassword;
+  }
+
+  private cargarCiudades(){
+    this.ciudades.push("Armenia");
+    this.ciudades.push("Calarcá");
+    this.ciudades.push("Pereira");
+    this.ciudades.push("Manizales");
+    this.ciudades.push("Medellín");
+    }
 }
